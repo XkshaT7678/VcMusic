10a35916f7c0ecf2649001b6ab7bed0e6f9b6ee8
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-01 09:29:36 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -13,6 +13,6 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 """
 
 REPO_TEXT = (
-    "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
+    "[Github](https://github.com/Akshat7678/VcMusic)"
     + " | [Group](t.me/TGVCSUPPORT)"
 )
